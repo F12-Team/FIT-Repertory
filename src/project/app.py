f5b24195702f2c5a1e9f0ed17e9a1ba247baadd0
lSenichl <baryshnikov.semen@mail.ru>
2020-12-27 22:04:20 +0300

    Begin of dinamic work

---
+++ src/project/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, url_for, make_response, session, redirect, flash
+from flask_login import login_required, current_user
+from mysql_db import MySQL
+import mysql.connector as connector
+
+app = Flask(__name__)
+application = app
+
+app.config.from_pyfile('config.py')
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor(named_tuple=True)
+
+    cursor.execute('SELECT * FROM Directions ORDER BY order_of_direction;')
+    directions = cursor.fetchall()
+    cursor.close()
+
+    projects = []
+
+    return render_template('index.html', directions=directions, projects=projects, direction_id=0)
+
+@app.route('/direction/<int:direction_id>')
+def direction(direction_id):
+    cursor = mysql.connection.cursor(named_tuple=True)
+
+    cursor.execute('SELECT * FROM Directions ORDER BY order_of_direction;')
+    directions = cursor.fetchall()
+
+    cursor.execute('SELECT id, name FROM Projects WHERE id_of_direction = %s ORDER BY likes DESC limit 10;', (direction_id,))
+    projects = cursor.fetchall()
+    cursor.close()
+
+    return render_template('index.html', directions=directions, projects=projects, direction_id=direction_id)
+
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    cursor = mysql.connection.cursor(named_tuple=True)
+
+    cursor.execute('''SELECT p.name as name_of_project, p.description as description_of_project, p.poster, p.video, p.likes, p.git, p.site, 
+                            s.year, s.autumn_or_spring,
+                            c.last_name, c.first_name, c.middle_name, c.description as description_of_curator,
+                            d.name as name_of_direction, d.description as description_of_direction
+                    FROM Projects AS p 
+                    JOIN Semesters AS s ON p.id_of_semestr=s.id 
+                    JOIN Curators AS c ON c.id = p.id_of_curator 
+                    JOIN Directions AS d ON d.id = p.id_of_direction
+                    WHERE p.id = %s;''', (project_id,))
+    projects = cursor.fetchone()
+
+    cursor.execute('SELECT t.role, s.last_name, s.first_name, s.middle_name, g.name as name_of_group  FROM Teams AS t JOIN Students AS s ON t.id_of_student = s.id JOIN Groups AS g ON s.id_of_group = g.id WHERE id_of_project = %s ORDER BY last_name;', (project_id,))
+    teams = cursor.fetchall()
+    cursor.close()
+
+    return render_template('project.html', projects=projects, teams=teams)
+
+@app.route('/direction/<int:direction_id>/all')
+def all_projects(direction_id):
+    cursor = mysql.connection.cursor(named_tuple=True)
+
+    cursor.execute('SELECT id, name FROM Projects WHERE id_of_direction = %s ORDER BY likes DESC;', (direction_id,))
+    projects = cursor.fetchall()
+    cursor.close()
+
+    return render_template('all_projects.html', projects=projects)
