1e80f0a149f992ab0880a0230cbea7096366db1d
lSenichl <baryshnikov.semen@mail.ru>
2021-06-02 13:41:14 +0300

    add teamlead bp, fix users policies

---
+++ src/app/teamlead.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from functools import wraps
+from sqlalchemy import exc
+from models import Direction, Group, Role, Status, Semester, Type, Student, User, Image, Info, Project
+
+bp = Blueprint('teamlead', __name__, url_prefix='/teamlead')
+
+@bp.route('/')
+def index():
+    return redirect(url_for('index'))
